e5dd204942b2703287cdd7f0b2641aa5e3f33fa9
Ken Nance <nancek00@techsistance.com>
2023-11-07 15:54:30 -0600

    Reformatting player page on search

--- src/routes/nhl24/search/[platform]/[clubId]/+page.svelte
+++ src/routes/nhl24/search/[platform]/[clubId]/+page.svelte
@@ -37,9 +37,59 @@
 </div>
 <div class="stat-card members">
     <h4>Members</h4>
-    <Grid>
         {#each prop("1",data.data).members as member, index}
-            <Row class="row" style="padding: 4px; {getdyncss(index)}">
+        <div class="member-container">
+
+        <div class="member-header">
+            <div class="member-header-name">
+                {member.name}
+            </div>
+            <div class="member-header-info">
+                {member.favoritePosition} | {member.record}
+            </div>
+        </div>
+        <div class="stat-block-container">
+            <div class="stat-block">
+               <div class="stat-label">
+                 GP
+              </div>
+              <div class="stat-value">
+                {member.gp} 
+              </div>
+           </div>   
+           <div class="stat-block">
+               <div class="stat-label">
+                 G
+              </div>
+              <div class="stat-value">
+                {member.goals} 
+              </div>
+           </div>
+           <div class="stat-block">
+               <div class="stat-label">
+                 A
+              </div>
+              <div class="stat-value">
+                {member.assists} 
+              </div>
+           </div>
+           <div class="stat-block">
+               <div class="stat-label">
+                 Points 
+              </div>
+              <div class="stat-value">
+                {member.points}
+              </div>
+           </div>
+           <div class="stat-block">
+               <div class="stat-label">
+                +/- 
+              </div>
+              <div class="stat-value">
+                {member.plusmin}
+              </div>
+           </div>
+            <!-- <Row class="row" style="padding: 4px; {getdyncss(index)}">
                 <Column><SinglStat label="Name" value={member.name} /></Column>
                 <Column><SinglStat label="Position" value={member.favoritePosition} /></Column>
                 <Column><SinglStat label="Record" value={member.record} /></Column>
@@ -50,18 +100,76 @@
                 <Column><SinglStat label="Faceoff %" value={member.fop} /></Column>
                 <Column><SinglStat label="Pen. Shot %" value={member.penaltyshotpct} /></Column>
                 <Column><SinglStat label="Dekes" value={`${member.dekesmade} of ${member.dekes}`} /></Column>
-            </Row>
+                <Column><SinglStat label="Brkawy %" value={member.breakawaypct} /></Column>
+            </Row> -->
+        </div>
+        </div>
+        
         {/each}
-    </Grid>
 </div>
 {/if}
 
 
 <style>
-    :global(.row) {
-        background-color: var(--row-bg-color,#eeeeee);
-    }
-    .stat-card {
-        width: 96%;
-    }
+
+    .stat-block-container {
+  background-color: #F6F6F6;
+  border-radius: 8px;
+  display: inline-flex;
+  margin-top: 12px;
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+
+}
+
+
+.stat-block:last-child {
+    border-right: 0;
+
+}
+.stat-block:first-child {
+}
+
+.stat-block {
+  min-width: 56px;
+  padding: 6px;
+  flex-grow: 1;
+  border-right: solid 1px gray;
+  justify-content: center;
+  align-items: center;
+}
+.stat-label {
+  font-weight: bolder;
+  font-size: bigger;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.stat-value {
+  display: flex;
+  align-items: center;
+  justify-content: center; 
+}
+.members {
+    display: flex;
+    flex-direction: column;
+}
+.member-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;;
+}
+.member-header-name {
+    font-size: large;
+    font-weight: bolder;
+}
+.member-container {
+    border: solid 1px gray;
+    margin: 8px;
+    padding: 16px;
+    border-radius: 8px;
+    background-color: #E3F1FC;
+}
 </style>
