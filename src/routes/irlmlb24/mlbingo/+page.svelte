c2ce5dd0be2eb13088873e02c457c9ec6fc03e4a
Ken Nance <nancek00@techsistance.com>
2024-04-01 09:35:16 -0500

    Added Pot Split

--- src/routes/irlmlb24/mlbingo/+page.svelte
+++ src/routes/irlmlb24/mlbingo/+page.svelte
@@ -1,17 +1,44 @@
 <script>
-	import { onMount } from 'svelte';
-
-
+import { onMount } from 'svelte';
 import { collection, db, doc, query, getDocs } from '../../../firebase.js';
-	import { prop, forEach, assoc, assocPath, path, defaultTo} from 'ramda';
+import { keys, prop, forEach, assoc, assocPath, length, defaultTo, range, groupBy, identity} from 'ramda';
 
     let documentSnapshots = null;
     let teams = [];
     let teamRuns = {};
     const season = "regularseason";
 
+    let last15 = "Noneo"
+    let potsplit_15 = [];
+
+    let potsplit_15Test = ["sd","cws","cws","nyy"]
+    let groupPotTest = {
+        "SD": ["SD"],
+        "CWS": ["CWS","CSS"],
+        "NYY": ["NYY"]
+    }
+    const nl_teams = [
+        "ARI",
+        "ATL",
+        "CHC",
+        "CIN",
+        "COL",
+        "LAD",
+        "MIA",
+        "MIL",
+        "NYM",
+        "OAK",
+        "PHI",
+        "PIT",
+        "SD",
+        "SF",
+        "STL",
+        "WSH"
+    ]
+
+    let groupPot = {};
+
     onMount(async () => {
-        console.log("om")
         const teamsCollection = collection(db,`/irl_mlb24/${season}/teams`)
         const teamCollectionQuery = await query(teamsCollection);
         const teamsSnapshot = await getDocs(teamCollectionQuery);
@@ -25,13 +52,27 @@ import { collection, db, doc, query, getDocs } from '../../../firebase.js';
 
             teamRuns = assoc(team.abbr, {runs: []})(teamRuns)
             runsSnapshot.forEach(x => {
-                console.log(`id: ${x.id}, count: ${x.data().count}`)
-                teamRuns = assocPath([team.abbr,x.id],x.data().count)(teamRuns)
+                const runCount = x.data().count
+                teamRuns = assocPath([team.abbr,x.id],runCount)(teamRuns)
+
+                if(x.id == 15 && runCount > 0) {
+                    last15 = team.abbr
+                    range(0,runCount).forEach(runElem => {
+
+                        potsplit_15 = [...potsplit_15,team.abbr]
+                        groupPot = groupBy(identity)(potsplit_15)
+                    })
+                }
 
             })
 
 
         })(teams)
+
+
+
+
+
 
 
     })
@@ -42,37 +83,83 @@ import { collection, db, doc, query, getDocs } from '../../../firebase.js';
         return defaultRuns((prop((runs).toString(),prop(team,tr))))
         // return path([team,runs],teamRuns)
     }
+
+    const potPerShare = (shares,total) => {
+        return total == 0 ? 0 : (2500 / total * shares).toFixed(0)
+    }
+
 </script>
-<h2>MLB Bingo 15</h2>
+<div class="mymobile">
+    <h2>MLB Bingo 15</h2>
 
-<table style="border: solid 1px green;">
-<tr>
-    <th colspan=2>Team</th>
-    {#each {length: 16} as _, i}
-        <th>{i}</th>
+    <table style="border: solid 1px green;">
+    <tr>
+        <th colspan=2>NL Team</th>
+        {#each {length: 16} as _, i}
+            <th>{i}</th>
+        {/each}
+    </tr>
+
+    {#each teams as team,i}
+    {#if nl_teams.indexOf(team.abbr) >= 0}
+    <tr>
+        <th> 
+            {team.abbr}
+        </th>
+        <th> 
+            {team.owner}
+        </th>
+        {#each {length: 16} as _, i}
+            {#if getRuns(team.abbr,(i).toString(),teamRuns) == 0}
+                <td style="background-color: red">x</td>
+            {:else}
+                <td style="background-color: green">{getRuns(team.abbr,(i).toString(),teamRuns)}</td>
+            {/if}
+
+        {/each}
+    </tr>
+    {/if}
     {/each}
-</tr>
-{#each teams as team,i}
-<tr>
-    <th> 
-        {team.abbr}
-    </th>
-    <th> 
-        {team.owner}
-    </th>
-    {#each {length: 16} as _, i}
-        {#if getRuns(team.abbr,(i).toString(),teamRuns) == 0}
-            <td style="background-color: red">x</td>
-        {:else}
-            <td style="background-color: green">{getRuns(team.abbr,(i).toString(),teamRuns)}</td>
-        {/if}
+    <tr>
+        <th colspan=2>AL Team</th>
+        {#each {length: 16} as _, i}
+            <th>{i}</th>
+        {/each}
+    </tr>
+    {#each teams as team,i}
+    {#if nl_teams.indexOf(team.abbr) < 0}
+    <tr>
+        <th> 
+            {team.abbr}
+        </th>
+        <th> 
+            {team.owner}
+        </th>
+        {#each {length: 16} as _, i}
+            {#if getRuns(team.abbr,(i).toString(),teamRuns) == 0}
+                <td style="background-color: red">x</td>
+            {:else}
+                <td style="background-color: green">{getRuns(team.abbr,(i).toString(),teamRuns)}</td>
+            {/if}
 
+        {/each}
+    </tr>
+    {/if}
     {/each}
-</tr>
-{/each}
-</table>
+    </table>
+    <br><br>
+
+    <h4>15 Runs Pot (Split 2,500 Points)</h4>
+    <table>
+    <tr><th>Team</th><th>Shares</th><th>Total</th></tr>
+    {#each keys(groupPot) as team}
+        <tr><td>{team}</td><td>{prop(team)(groupPot).length}</td><td>{potPerShare(prop(team,groupPot).length,potsplit_15.length)} </td></tr>
+    {/each}
+    </table>
 
 
+
+</div>
 <style>
     tr {
         border: solid 1px black;
@@ -81,5 +168,9 @@ import { collection, db, doc, query, getDocs } from '../../../firebase.js';
     td, th {
         padding: 8px;
         border: solid 1px black;
+    }
+
+    .mymobile {
+        padding: 12px;
     }
 </style>
